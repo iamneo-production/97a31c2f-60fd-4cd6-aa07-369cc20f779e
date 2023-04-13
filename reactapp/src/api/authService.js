44d823cb189b07982c29df5111d46082670c60c2
saivigneesh456 <sai.9441051886@gmail.com>
2023-04-13 18:25:56 +0000

    Merge branch 'main' into PG_Portal_Admission--saivigneesh456

--- reactapp/src/api/authService.js
+++ reactapp/src/api/authService.js
@@ -1,4 +1,3 @@
-
 const baseUrl = "https://8080-deacebeebcbbfafccddecaeebaeccc.project.examly.io";
 
 const register = async (data) => { 
@@ -10,7 +9,7 @@ const register = async (data) => {
         mobileNumber: data.mobileNumber,
     } 
     console.log(formatData);
-    if (formatData.userRole === "admin")
+    if (formatData.userRole === "Admin")
         return fetch(`${baseUrl}/admin/signup`, {
             method: "POST",
             headers: {
@@ -26,45 +25,17 @@ const register = async (data) => {
             },
             body: JSON.stringify(data),
         });
-};
+}
 
-const myHeaders = new Headers({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://8080-deacebeebfccecfccdceceefcbafdfb.examlyiopb.examly.io/',
-    'Access-Control-Allow-Methods' : 'OPTIONS, DELETE, POST, GET, PATCH, PUT'
-  });
-
-let login = async (data) => {
-      let res = await fetch(`${baseUrl}/user/login`, {
-        headers: myHeaders,
+const login = async (data) => { 
+    const response = await fetch(`${baseUrl}/user/login`, {
         method: "POST",
-        body: JSON.stringify({      
-         email: data.email,
-         password: data.password,
-        }),
-      });
-      if (!res.ok) {
-        const message = `An error has occured: ${res.status}`;
-        throw new Error(message);
-      }
-      let resJson = await res.json();
-      console.log(res.status);
-      return res;
-    }
-
-
-  
-
-// const login = async (data) => { 
-//     let response = await fetch(`${baseUrl}/user/login`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(data),
-//     });
-//     console.log(response)
-//     return response.json();
-// }
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
 
 export default { register, login };
