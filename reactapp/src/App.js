16b34a422712680a51f894bdc951654f37def0b8
saivigneesh456 <sai.9441051886@gmail.com>
2023-04-25 06:50:21 +0000

    RESOLVED THE CONFILCTS

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router,Route,Routes,Navigate } from 'react-router-dom';
+import { UseAuthContext } from './hooks/UseAuthContext';
+import Signup from './components/Auth/Signup/Signup';
+import Login from './components/Auth/Login/Login';
+import AdminHomePage from './components/Admin/AdminHomePage/AdminHomePage';
+import Homepage from './components/User/Homepage/Homepage';
 import './App.css';
 
 function App() {
+  const { user } = UseAuthContext()
+
+  let redirectPath = "";
+  if (!user) {
+    redirectPath = "/";
+  } else if (user.roles === "admin") {
+    redirectPath = "/admin/dashboard";
+  } else {
+    redirectPath = "/user/dashboard";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={!user ? <Signup /> : <Navigate to={redirectPath} />} />
+        <Route path="/login"element={!user ? <Login /> : <Navigate to={redirectPath} />} />
+        <Route path="/" element={!user ? <Login /> : <Navigate to={redirectPath} />} />
+        <Route path="/admin/dashboard" element={!user ? (<Navigate to="/" />) :
+            user.roles === "admin" ? (<AdminHomePage /> ) : ( <Navigate to="/user/dashboard" /> ) }/>
+        <Route path="/user/dashboard" element={!user ? (<Navigate to="/" />) :
+           user.roles === "admin" ? (<AdminHomePage />) : (<Homepage />)} />
+        <Route path="*" element={<>404 no such page go to home page</>} />
+      </Routes>
+    </Router>
+
   );
 }
 
