2e4d6eeea5f51b12128b8f6728241fe6a6061046
shaiksafeena <safeenashaik06@gmail.com>
2023-05-01 15:36:14 +0000

    changes done

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -9,7 +9,9 @@ import Enrollcourse from './components/User/HomePage/Enrollcourse';
 import Viewacademy from './components/User/HomePage/Viewacademy';
 import './App.css';
 import Enrolledcourse from './components/User/Enrolledcourse';
-
+import Course from './components/Admin/Course/Course';
+import Viewcourse from './components/Admin/Course/Viewcourse';
+import Editcourse from './components/Admin/Course/Editcourse';
 function App() {
   const { user } = UseAuthContext()
 
@@ -32,14 +34,14 @@ function App() {
             user.roles === "admin" ? (<AdminHomePage /> ) : ( <Navigate to="/user/dashboard" /> ) }/>
         <Route path="/user/dashboard" element={!user ? (<Navigate to="/" />) :
            user.roles === "admin" ? (<AdminHomePage />) : (<Navpage />)} />
-        <Route path="/Enrollcourse" element={<Enrollcourse/>}/>
-        <Route path="/Viewacademy" element={<Viewacademy/>}/>
-        {/* <Route path="/Course" element={<Course/>}/> */}
+        <Route path="/Enrollcourse" element={!user ? <Navigate to="/" /> :<Enrollcourse/>}/>
+        <Route path="/Viewacademy" element={!user ? <Navigate to="/" /> :<Viewacademy/>}/>
+        <Route path="/UserCourse" element={!user ? <Navigate to="/" /> :<UserCourse/>}/>
+        <Route path="/Enrolledcourse" element={!user? <Navigate to="/" /> :<Enrolledcourse/>}/>
         <Route path="*" element={<>404 no such page go to home page</>} />
-        <Route path="/admin/addCourse" element={!user ? <Navigate to="/" />:<Course />} />
-          <Route path="/admin/viewCourse" element={!user ? <Navigate to="/" />:<Viewcourse/>}/>
-          <Route path="/admin/editCourse/:id" element={!user ? <Navigate to="/" />:<Editcourse />} />
-          <Route path="/admin/Viewstudent" element={<Adminstudent />} />
+        <Route path="/admin/addCourse" element={!user ? <Navigate to="/" />:<Course />} /> 
+        <Route path="/admin/viewCourse" element={!user ? <Navigate to="/" />:<Viewcourse/>}/>
+         <Route path="/admin/editCourse/:id" element={!user ? <Navigate to="/" />:<Editcourse />} />
       </Routes>
     </Router>
   );
