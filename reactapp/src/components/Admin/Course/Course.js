f7ce0b2603c80965b496f74ccb412b2e4ead6559
shaiksafeena <safeenashaik06@gmail.com>
2023-05-08 06:44:15 +0000

    changes done

--- reactapp/src/components/Admin/Course/Course.js
+++ reactapp/src/components/Admin/Course/Course.js
@@ -1,7 +1,7 @@
 import React,{ useState } from 'react'
 import '../Course/Course.css';
-import { useNavigate, Link } from "react-router-dom";
-import { addCourse} from "../../../api/courseApi.js";
+import { useNavigate,Link } from "react-router-dom";
+import { addCourse, getCourses} from "../../../api/courseApi.js";
 import NavBar from '../Navbar/Navbar.js';
 import { AdminGuard } from "../../../AuthGuard/AdminGuard"
 
@@ -16,14 +16,12 @@ const Course = () => {
   const navigate = useNavigate();
 
   const handleClick = (event)=>{
-    handleSubmit(event).then((data) => {
-      console.log(data)
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
+    handleSubmit(event);
+    fetchData();
     navigate('/admin/viewCourse');
+  }
+  const fetchData=async()=>{
+    const data = await getCourses();
   }
   const handleSubmit = async(event) => {
     event.preventDefault();
