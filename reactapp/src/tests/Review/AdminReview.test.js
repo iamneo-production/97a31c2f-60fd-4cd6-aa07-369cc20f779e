d8f6077680795e552aeab793638c909d9ab9887b
KallipudiGowtham <114467107+KallipudiGowtham@users.noreply.github.com>
2023-05-10 19:49:42 +0530

    Revert "Admission portal  yashaswi.a"

--- reactapp/src/tests/Review/AdminReview.test.js
+++ reactapp/src/tests/Review/AdminReview.test.js
@@ -1,25 +1,17 @@
-import {
-  getByTestId,
-  queryByTestId,
-  render,
-  screen,
-} from "@testing-library/react";
-import React from "react";
-import { MemoryRouter } from "react-router";
-import Review from "../../components/Admin/Review/Review";
+import { getByTestId, queryByTestId, render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import Review from '../../components/Admin/Review/Review';
 
-describe("Review Component", () => {
-  render(
-    <MemoryRouter>
-      <Review />
-    </MemoryRouter>
-  );
+describe('Review Component', () => {
+    render(<MemoryRouter><Review /></MemoryRouter>);
 
-  test("fe_react_adminReview", () => {
-    const userName = screen.queryByTestId("userName");
-    const comments = screen.queryByTestId("comments");
+    test('fe_react_adminReview', () => {
+        const userName = screen.queryByTestId('userName');
+        const comments = screen.queryByTestId('comments');
 
-    expect(userName).toBeTruthy();
-    expect(comments).toBeTruthy();
-  });
-});
+        expect(userName).toBeTruthy();
+        expect(comments).toBeTruthy();
+    })
+    
+})
