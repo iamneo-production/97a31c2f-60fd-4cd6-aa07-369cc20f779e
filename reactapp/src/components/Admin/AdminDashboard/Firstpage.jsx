49aaa05fbb2c3ae0e6dc0c398a83a5016cdfc23a
AdapaYashaswi <adapayashaswi@gmail.com>
2023-05-16 15:45:40 +0000

    admin dashboard

--- reactapp/src/components/Admin/AdminDashboard/Firstpage.jsx
+++ reactapp/src/components/Admin/AdminDashboard/Firstpage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import NavBar from '../Navbar/Navbar'
-import { useNavigate } from "react-router";
+import { NavLink,useNavigate } from "react-router-dom";
 import { store } from "../../../store";
 import './Firstpage.css'
-import { AdminGuard } from '../../../AuthGuard/AdminGuard';
-const Firstpage = () => {
+import { AdminGuard} from "../../../AuthGuard/AdminGuard"
+const Navpage = () => {
     const navigate = useNavigate();
     const handleLogout = () => {
         store.dispatch({ type: "LOGOUT" });
@@ -14,35 +13,48 @@ const Firstpage = () => {
 
     return (
         <AdminGuard>
-            <NavBar/>
 
-            {/* <div className="nvbar" data-testid="user_navbar">
-                
+            <div className="nv" data-testid="user_navbar">
+                <div className='top'>
+                    <div>
+                        <h2>PG Admission</h2>
+                    </div>
+                    <div className='first'>
+                        <div className="link">
+                            <NavLink to="/admin/dashboard">Institute   &nbsp;  &nbsp;  &nbsp;</NavLink>
+                        </div>
 
-                <h2>PG Admission</h2>
+                        <div className="link">
+                            <Link to="/admin/viewCourse">Course   &nbsp;  &nbsp;  &nbsp;</Link>
+                        </div>
 
-                <div className="link">
-                    <Link to="/Viewacademy">Institute</Link>
-                </div>
-                <div className="link">
-                    <Link to="/Enrolledcourse">Enrolled course</Link>
-                </div>
-                <div className="link">
-                    <Link to="/FeedBack">FeedBack</Link>
-                </div>
-                <div className="out">
-                    <button data-testid="logout" name='logout' onClick={handleLogout} >Logout</button>
-                </div>
-            </div> */}
+                        <div className="link">
+                            <Link to="/admin/Viewstudent">Students   &nbsp;  &nbsp;  &nbsp;</Link>
+                        </div>
 
-            <div className="image">
+                        <div className="link">
+                            <Link to="/FeedBack">FeedBack</Link>
+                        </div>
+                    </div>
+                    <div className="out">
+                        <button data-testid="logout" name='logout' onClick={handleLogout} >Logout</button>
+                    </div>
+                </div>
+
                 <div className='txt'>
-                    <h1>WELCOME TO PG ADMISSION PORTAL!!!</h1>
-                    <h6>Navigate to your future with us</h6>
+                    <p>WELCOME TO </p>
+                    <h2>PG Admission Portal</h2>
+                    <h4>Bright Future Ahead </h4>
+                </div>
+                <div className='addon'>
+                    <h1>BOOST YOUR CAREER WITH US</h1>
+                    <p>we provide you a accessible virtual platform to check over various institutes,courses and let's you to chooose the best for you. We belive that exploring through neumerous institutions is important before getting into any course, This platform let's you to explore.And now it's time to CHOOSE YOUR'S!!!.</p>
+                </div>
+                <div className='last'>
+                    <h1><span> </span></h1>
                 </div>
             </div>
-
-        </AdminGuard>
+        </AdminGuard >
     );
 }
-export default Firstpage
+export default Navpage
