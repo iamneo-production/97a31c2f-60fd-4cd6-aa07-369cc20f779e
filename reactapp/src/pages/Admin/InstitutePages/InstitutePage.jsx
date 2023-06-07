d89580083aae3a3a014d03dd812b161b029adfcb
prakash-aathi <prakashaathi321@gmail.com>
2023-06-07 16:36:48 +0000

    ➕ Created Dynamic Nav bar

--- reactapp/src/pages/Admin/InstitutePages/InstitutePage.jsx
+++ reactapp/src/pages/Admin/InstitutePages/InstitutePage.jsx
@@ -1,10 +1,13 @@
 import {AdminGuard} from '../../../AuthGuard/AdminGuard';
 import AdminHomePage from '../../../components/Admin/AdminHomePage/AdminHomePage'
+import Navbar from '../../../components/Navbar/Navbar'
+
 
 const InstitutePage = () => {
     return (
     <>
       <AdminGuard>
+        <Navbar />
         <AdminHomePage>
         </AdminHomePage>
       </AdminGuard>
