d0a8664ed9d0eaeb2e909aac365d961a0260db0f
saivigneesh456 <sai.9441051886@gmail.com>
2023-06-16 18:33:50 +0000

    updated websecurity

--- reactapp/src/api/authService.js
+++ reactapp/src/api/authService.js
@@ -1,5 +1,5 @@
 // Define the base URL for the API requests
-export const baseUrl = "https://8080-fccfeeaccfaaabbebcbbfafccddecaeebaeccc.project.examly.io";
+export const baseUrl = "https://8080-deacebeebcbbfafccddecaeebaeccc.project.examly.io";
 
 // Define a function to register a user
 const register = async (data) => {
