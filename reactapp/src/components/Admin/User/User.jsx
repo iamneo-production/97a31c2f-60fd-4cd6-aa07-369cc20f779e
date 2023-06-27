2e8c8274e1301752ba9cd71b489c7ecc12967f2e
prakash-aathi <prakashaathi321@gmail.com>
2023-06-27 04:27:04 +0000

    seperated child component

--- reactapp/src/components/Admin/User/User.jsx
+++ reactapp/src/components/Admin/User/User.jsx
@@ -5,6 +5,7 @@ import { useNavigate, useParams, NavLink, Link } from "react-router-dom";
 import { baseUrl } from "../../../api/authService";
 import './AdminStudent.css';
 import Navbar from "../Navbar/Navbar";
+import ViewStudent from "./ViewStudent";
 
 
 let auth = "";
@@ -108,7 +109,6 @@ const AdminStudent1 = () => {
       const userDatabase = await fetchUserDatabase();
       setFetchedStudentData(data.concat(userDatabase));
       setStudentData(data.concat(userDatabase));
-      console.log(data.concat(userDatabase), ".....................")
       setIsLoading(false);
       setIsError({ state: false, msg: "" });
       if (response.status === 400) {
@@ -206,124 +206,7 @@ const AdminStudent1 = () => {
 
   const StudentComponent = () => {
     return (
-      <>
-        <div className="p-4 px-4 mx-10 mt-12  rounded-xl">
-          <div className="flex justify-center text-green-500">
-            <h2 className="text-2xl fa-solid fa-users-line mt-4 mb-4 ">Student Details</h2>
-          </div>
-          <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
-            <div>
-              <div className="flex">
-                <p className="font-bold">ID:</p>
-                <p>{passStudentData.id}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Student ID Number:</p>
-                <p>{passStudentData.studentIdNumber}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">First Name:</p>
-                <p>{passStudentData.firstName}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Last Name:</p>
-                <p>{passStudentData.lastName}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Email ID: </p>
-                <p>{passStudentData.emailId}</p>
-              </div>
-
-              <div className="flex">
-                <p className="font-bold">Course ID: </p>
-                <p>{passStudentData.courseId}</p>
-              </div>
-
-
-
-            </div>
-
-            <div>
-
-              <div className="flex">
-                <p className="font-bold">Student DOB:</p>
-                <p>{new Date(passStudentData.studentDOB).toLocaleDateString()}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Father's Name: </p>
-                <p>{passStudentData.fatherName}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Mother's Name:</p>
-                <p>{passStudentData.motherName}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Phone Number 1:</p>
-                <p>{passStudentData.phoneNumber1}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Phone Number 2:</p>
-                <p>{passStudentData.phoneNumber2}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid md:grid-cols-2 grid-cols-1  gap-4 mt-4">
-            <div>
-              <div className="flex">
-                <p className="font-bold">SSLC:</p>
-                <p>{passStudentData.sslc}</p>
-              </div>
-
-              <div className="flex">
-                <p className="font-bold">HSC:</p>
-                <p>{passStudentData.hsc}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Diploma: </p>
-                <p>{passStudentData.diploma ? "Yes" : "No"}</p>
-              </div>
-
-            </div>
-
-            <div>
-              <div className="flex">
-                <p className="font-bold">House Number:</p>
-                <p>{passStudentData.houseNumber}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Street Name:</p>
-                <p>{passStudentData.streetName}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold ">Area Name</p>
-                <p >: {passStudentData.areaName}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">State:</p>
-                <p>{passStudentData.state}</p>
-              </div>
-
-              <div className="flex">
-                <p className="font-bold">Pincode:</p>
-                <p>{passStudentData.pincode}</p>
-              </div>
-              <div className="flex">
-                <p className="font-bold">Nationality:</p>
-                <p>{passStudentData.nationality}</p>
-              </div>
-
-
-            </div>
-          </div>
-
-          {/* /back button */}
-          <button onClick={handleBackList} className="md:mx-10 mt-2 px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-xl mb-32">
-            back
-          </button>
-        </div>
-
-      </>
+      <ViewStudent handleBackList={handleBackList} passStudentData={passStudentData}  />
     )
   }
 
@@ -417,7 +300,6 @@ const AdminStudent1 = () => {
                 const course = course1.find((eachCourse) => {
                   return eachCourse.courseId == courseId;
                 });
-                console.log(index);
 
                 return (
                   <>
