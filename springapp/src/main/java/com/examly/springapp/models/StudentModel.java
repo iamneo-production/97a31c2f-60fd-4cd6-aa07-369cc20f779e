f032db2346b819fbfd21a31cb04cb0b65a59b105
saivigneesh456 <sai.9441051886@gmail.com>
2023-07-29 14:34:55 +0000

    merging main branch

--- springapp/src/main/java/com/examly/springapp/models/StudentModel.java
+++ springapp/src/main/java/com/examly/springapp/models/StudentModel.java
@@ -6,16 +6,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "Student")
 
 public class StudentModel {
@@ -42,4 +34,133 @@ public class StudentModel {
     private String eligibility;
     private String courseId;
     private String instituteId;
+    
+    public Integer getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getFatherName() {
+        return fatherName;
+    }
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+    public String getMotherName() {
+        return motherName;
+    }
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+    public String getPhoneNumber1() {
+        return phoneNumber1;
+    }
+    public void setPhoneNumber1(String phoneNumber1) {
+        this.phoneNumber1 = phoneNumber1;
+    }
+    public String getPhoneNumber2() {
+        return phoneNumber2;
+    }
+    public void setPhoneNumber2(String phoneNumber2) {
+        this.phoneNumber2 = phoneNumber2;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public Date getStudentDOB() {
+        return studentDOB;
+    }
+    public void setStudentDOB(Date studentDOB) {
+        this.studentDOB = studentDOB;
+    }
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+    public String getStreetName() {
+        return streetName;
+    }
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+    public String getAreaName() {
+        return areaName;
+    }
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public Integer getPincode() {
+        return pincode;
+    }
+    public void setPincode(Integer pincode) {
+        this.pincode = pincode;
+    }
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public Integer getSslc() {
+        return sslc;
+    }
+    public void setSslc(Integer sslc) {
+        this.sslc = sslc;
+    }
+    public Integer getHsc() {
+        return hsc;
+    }
+    public void setHsc(Integer hsc) {
+        this.hsc = hsc;
+    }
+    public Integer getDiploma() {
+        return diploma;
+    }
+    public void setDiploma(Integer diploma) {
+        this.diploma = diploma;
+    }
+    public String getEligibility() {
+        return eligibility;
+    }
+    public void setEligibility(String eligibility) {
+        this.eligibility = eligibility;
+    }
+    public String getCourseId() {
+        return courseId;
+    }
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+    public String getInstituteId() {
+        return instituteId;
+    }
+    public void setInstituteId(String instituteId) {
+        this.instituteId = instituteId;
+    }
+
+    
 }
